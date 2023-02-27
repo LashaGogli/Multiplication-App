381e9a7fdb0fb2afa708c048f1039ff652808ac6
Lasha <lashagoglidzelasha@gmail.com>
2023-02-27 20:24:30 +0400

    Final Version

--- main.js
+++ main.js
@@ -3,36 +3,56 @@ let somediv = document.querySelector(".somediv");
 let scorediv = document.querySelector(".scorediv");
 let buttondiv = document.querySelector(".buttondiv");
 let inputDiv = document.querySelector(".inputDiv");
+let imennascore = document.querySelector(".imennascore");
+let img1 = document.querySelector(".img1");
+let img2 = document.querySelector(".img2");
+let img3 = document.querySelector(".img3");
+let imgDivs = document.querySelector(".imgDivs");
+let endgame = document.createElement("p");
+endgame.classList.add("endgame");
+let newGame = document.createElement("button");
+newGame.classList.add("newGame");
 
 
 let x = Math.floor(Math.random() * 21);
 let y = Math.floor(Math.random() * 21);
 let score = parseInt(localStorage.getItem("score")) || 0;
+let countlive = parseInt(localStorage.getItem("countlive")) || 3;
 
 
+
+
+if (countlive == 2) {
+    img3.style.display = "none";
+} else if (countlive == 1) {
+    img3.style.display = "none";
+    img2.style.display = "none";
+}
 
 somediv.innerHTML = "What is " + x + " multiply by " + y + " ";
-scorediv.innerHTML = "score: " + score;
+imennascore.innerHTML = "score: " + score;
 
 
 buttondiv.addEventListener("click", function () {
     if (inputDiv.value == x * y) {
         score++;
 
+
     } else {
-        score--;
+        countlive--;
+        localStorage.setItem("countlive", countlive.toString());
+
+
+
     }
-    scorediv.innerHTML = "score: " + score;
+    imennascore.innerHTML = "score: " + score;
     localStorage.setItem("score", score.toString());
     location.reload();
 
 
 
 })
-let endgame = document.createElement("p");
-endgame.classList.add("endgame");
-let newGame = document.createElement("button");
-newGame.classList.add("newGame");
+
 
 
 if (score == 10) {
@@ -41,65 +61,42 @@ if (score == 10) {
     endgame.innerHTML = "You Won!";
     insidediv.appendChild(newGame);
     newGame.innerHTML = "New Game";
-} else if (score == -3) {
+    localStorage.setItem("score", "0");
+    localStorage.setItem("countlive", "3");
+
+
+
+
+} else if (localStorage.getItem("countlive") == 0) {
     insidediv.innerHTML = "";
     insidediv.appendChild(endgame);
     endgame.innerHTML = "Game Over!";
     insidediv.appendChild(newGame);
     newGame.innerHTML = "New Game";
+    localStorage.setItem("countlive", "3");
+    localStorage.setItem("score", "0");
+
+
+
 
 }
 
 
 newGame.addEventListener("click", function () {
-    score = 0;
+
+    score = parseInt(localStorage.getItem("score"));
+    countlive = parseInt(localStorage.getItem("countlive"));
+
     insidediv.innerHTML = "";
-    let scorediv2 = document.createElement("div");
-    scorediv2.classList.add("scorediv");
-    insidediv.appendChild(scorediv2);
-    scorediv2.innerHTML = "score: " + score;
-    let somediv2 = document.createElement("div");
-    somediv2.classList.add("somediv");
-    insidediv.appendChild(somediv2);
-    somediv2.innerHTML = "What is " + x + " multiply by " + y + " ";
-    let inputDiv2 = document.createElement("input");
-    inputDiv2.classList.add("inputDiv");
-    insidediv.appendChild(inputDiv2);
-    inputDiv2.placeholder = "Enter your answer";
-    let buttondiv2 = document.createElement("button");
-    buttondiv2.classList.add("buttondiv");
-    insidediv.appendChild(buttondiv2);
-    buttondiv2.innerHTML = "Submit";
-
-
-    buttondiv2.addEventListener("click", function () {
-        if (inputDiv.value == x * y) {
-            score++;
-    
-        } else {
-            score--;
-        }
-        scorediv2.innerHTML = "score: " + score;
-        localStorage.setItem("score", score.toString());
-        location.reload();
-    
-    
-    
-    })
-    if (score == 10) {
-        insidediv.innerHTML = "";
-        insidediv.appendChild(endgame);
-        endgame.innerHTML = "You Won!";
-        insidediv.appendChild(newGame);
-        newGame.innerHTML = "New Game";
-    } else if (score == -3) {
-        insidediv.innerHTML = "";
-        insidediv.appendChild(endgame);
-        endgame.innerHTML = "Game Over!";
-        insidediv.appendChild(newGame);
-        newGame.innerHTML = "New Game";
-    
-    }
+    insidediv.appendChild(scorediv);
+    insidediv.appendChild(somediv);
+    insidediv.appendChild(inputDiv);
+    insidediv.appendChild(buttondiv);
+    somediv.innerHTML = "What is " + x + " multiply by " + y + " ";
+    imennascore.innerHTML = "score: " + score;
+    img1.style.display = "block";
+    img2.style.display = "block";
+    img3.style.display = "block";
 
 
 
